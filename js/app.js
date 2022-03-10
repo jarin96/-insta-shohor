93c1f067d94630676752d19741433b8d3417e8d0
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-03-10 21:19:44 +0600

    header user profile picture debugging solve

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  console.log(likedPostsId);
   likedPostsId.push(id);
   showPosts(posts);
 };
@@ -53,6 +52,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
+  const userImage = post.userImage;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -63,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
